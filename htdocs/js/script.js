1b222f26a2a560180785feae423ab19b05e1951d
Katerina Wang <yusie@live.cn>
2025-12-22 15:32:15 -0700

    changed font, added typed description.

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,29 +1,8 @@
-let left, bio;
-let lastKnownScrollPosition = 0;
-let ticking = false;
-
-window.onload = function (){
-    // left = document.querySelector(".left");
-    // bio = document.createElement("img");
-    // bio.src = "../img/avatar.gif";
-    // bio.alt = "bio";
-    // left.appendChild(bio);
-
-    // document.addEventListener("scroll", (event) => {
-    //     lastKnownScrollPosition = window.scrollY;
-    //
-    //     if (!ticking) {
-    //         window.requestAnimationFrame(() => {
-    //             doSomething(lastKnownScrollPosition);
-    //             ticking = false;
-    //         });
-    //
-    //         ticking = true;
-    //     }
-    // });
-}
-
-function doSomething(scrollPos) {
-    left.style.display = scrollPos > 0 ? "none" : "block";
-}
-
+window.addEventListener("load", () => {
+    const typed = new Typed("#typed", {
+        stringsElement: "#typed-desc",
+        typeSpeed: 50,
+        loop: true,
+        loopCount: Infinity,
+    });
+});
