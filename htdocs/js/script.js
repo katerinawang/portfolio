cff59aa7e1c94d443043cdd9f7ab97304579e433
Katerina Wang <yusie@live.cn>
2025-12-22 04:20:45 -0700

    added avatar.gif

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,29 +1,29 @@
-let right, bio;
+let left, bio;
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
 window.onload = function (){
-    right = document.querySelector(".right");
-    bio = document.createElement("img");
-    bio.src = "../img/bio.jpeg";
-    bio.alt = "bio";
-    right.appendChild(bio);
+    // left = document.querySelector(".left");
+    // bio = document.createElement("img");
+    // bio.src = "../img/avatar.gif";
+    // bio.alt = "bio";
+    // left.appendChild(bio);
 
-    document.addEventListener("scroll", (event) => {
-        lastKnownScrollPosition = window.scrollY;
-
-        if (!ticking) {
-            window.requestAnimationFrame(() => {
-                doSomething(lastKnownScrollPosition);
-                ticking = false;
-            });
-
-            ticking = true;
-        }
-    });
+    // document.addEventListener("scroll", (event) => {
+    //     lastKnownScrollPosition = window.scrollY;
+    //
+    //     if (!ticking) {
+    //         window.requestAnimationFrame(() => {
+    //             doSomething(lastKnownScrollPosition);
+    //             ticking = false;
+    //         });
+    //
+    //         ticking = true;
+    //     }
+    // });
 }
 
 function doSomething(scrollPos) {
-    right.style.display = scrollPos > 0 ? "none" : "block";
+    left.style.display = scrollPos > 0 ? "none" : "block";
 }
 
